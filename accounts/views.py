f1410792830f4075caeedeaf525a815679014422
Alex Pereira <hwaeeting0104@gmail.com>
2025-02-17 05:29:43 +0900

    update : register,  login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "username": user.username,
+                "nickname": user.nickname,
+                "roles": [{"role": user.role}]  # roles 포함하여 응답
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["nickname"] = user.nickname  # 닉네임도 JWT에 포함
+        return token
+
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
